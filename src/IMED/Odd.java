889ce78523fffaac69d6b3645504d524689d95b1
neehhaa <nehawasnik95@outlook.com>
2022-07-13 14:55:29 +0530

    Odd Number

--- src/IMED/Odd.java
+++ src/IMED/Odd.java
@@ -2,5 +2,17 @@
 
 package IMED;
 
+import java.util.Scanner;
 public class Odd {
+    public static void main(String[] args) {
+        System.out.println("Enter a Number : ");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if (num % 2 == 1) {
+            System.out.println("IT IS ODD!!!");
+        }
+        else{
+            System.out.println("NOT AN ODD NUMBER!!!");
+        }
+    }
 }
